939bf41e2646590afcd6205e9d56c2b5610a0c55
Amanda Guan <ag.amandaguan@gmail.com>
2023-06-12 21:24:46 -0700

    add recipecontroller

--- RecipeBox/Controllers/HomeController.cs
+++ RecipeBox/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using RecipeBox.Models;
-using System.Collections.Generic;
 
 namespace RecipeBox.Controllers
 {
@@ -10,22 +8,7 @@ namespace RecipeBox.Controllers
         [HttpGet("/")]
         public ActionResult Index()
         {
-
-            List<Recipe> allRecipes = Recipe.GetAll();
-            return View(allRecipes);
-        }
-
-        [HttpGet("/recipes/new")]
-        public ActionResult CreateForm()
-        {
             return View();
-        }
-
-        [HttpPost("/recipes")]
-        public ActionResult Create(string description)
-        {
-            Recipe myRecipe = new Recipe(description);
-            return RedirectToAction("Index");
         }
 
     }
---
+++ RecipeBox/Controllers/RecipeController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using RecipeBox.Models;
+using System.Collections.Generic;
+
+namespace RecipeBox.Controllers
+{
+    public class RecipesController : Controller
+    {
+
+        [HttpGet("/recipes")]
+        public ActionResult Index()
+        {
+            List<Recipe> allRecipes = Recipe.GetAll();
+            return View(allRecipes);
+        }
+
+        [HttpGet("/recipes/new")]
+        public ActionResult CreateForm()
+        {
+            return View();
+        }
+
+        [HttpPost("/recipes")]
+        public ActionResult Create(string description)
+        {
+            Recipe myRecipe = new Recipe(description);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
