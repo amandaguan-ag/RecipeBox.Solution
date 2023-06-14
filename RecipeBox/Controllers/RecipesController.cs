54dd5462f3a282b14a18493faf7d401a384eafce
Amanda Guan <ag.amandaguan@gmail.com>
2023-06-13 18:36:29 -0700

    setup efcore

--- RecipeBox/Controllers/RecipesController.cs
+++ RecipeBox/Controllers/RecipesController.cs
@@ -1,28 +1,28 @@
-using Microsoft.AspNetCore.Mvc;
-using RecipeBox.Models;
-using System.Collections.Generic;
+// using Microsoft.AspNetCore.Mvc;
+// using RecipeBox.Models;
+// using System.Collections.Generic;
 
-namespace RecipeBox.Controllers
-{
-    public class RecipesController : Controller
-    {
+// namespace RecipeBox.Controllers
+// {
+//     public class RecipesController : Controller
+//     {
 
-        [HttpGet("/categories/{categoryId}/recipes/new")]
-        public ActionResult New(int categoryId)
-        {
-            Category category = Category.Find(categoryId);
-            return View(category);
-        }
+//         [HttpGet("/categories/{categoryId}/recipes/new")]
+//         public ActionResult New(int categoryId)
+//         {
+//             Category category = Category.Find(categoryId);
+//             return View(category);
+//         }
 
-        [HttpGet("/categories/{categoryId}/recipes/{recipeId}")]
-        public ActionResult Show(int categoryId, int recipeId)
-        {
-            Recipe recipe = Recipe.Find(recipeId);
-            Category category = Category.Find(categoryId);
-            Dictionary<string, object> model = new Dictionary<string, object>();
-            model.Add("recipe", recipe);
-            model.Add("category", category);
-            return View(model);
-        }
-    }
-}
+//         [HttpGet("/categories/{categoryId}/recipes/{recipeId}")]
+//         public ActionResult Show(int categoryId, int recipeId)
+//         {
+//             Recipe recipe = Recipe.Find(recipeId);
+//             Category category = Category.Find(categoryId);
+//             Dictionary<string, object> model = new Dictionary<string, object>();
+//             model.Add("recipe", recipe);
+//             model.Add("category", category);
+//             return View(model);
+//         }
+//     }
+// }
--- RecipeBox/Models/Category.cs
+++ RecipeBox/Models/Category.cs
@@ -1,41 +1,41 @@
-using System.Collections.Generic;
+// using System.Collections.Generic;
 
-namespace RecipeBox.Models
-{
-    public class Category
-    {
-        private static List<Category> _instances = new List<Category> { };
-        public string Name { get; set; }
-        public int Id { get; }
-        public List<Recipe> Recipes { get; set; }
+// namespace RecipeBox.Models
+// {
+//     public class Category
+//     {
+//         private static List<Category> _instances = new List<Category> { };
+//         public string Name { get; set; }
+//         public int Id { get; }
+//         public List<Recipe> Recipes { get; set; }
 
-        public Category(string categoryName)
-        {
-            Name = categoryName;
-            _instances.Add(this);
-            Id = _instances.Count;
-            Recipes = new List<Recipe> { };
-        }
+//         public Category(string categoryName)
+//         {
+//             Name = categoryName;
+//             _instances.Add(this);
+//             Id = _instances.Count;
+//             Recipes = new List<Recipe> { };
+//         }
 
-        public static void ClearAll()
-        {
-            _instances.Clear();
-        }
+//         public static void ClearAll()
+//         {
+//             _instances.Clear();
+//         }
 
-        public static List<Category> GetAll()
-        {
-            return _instances;
-        }
+//         public static List<Category> GetAll()
+//         {
+//             return _instances;
+//         }
 
-        public static Category Find(int searchId)
-        {
-            return _instances[searchId - 1];
-        }
+//         public static Category Find(int searchId)
+//         {
+//             return _instances[searchId - 1];
+//         }
 
-        public void AddRecipe(Recipe recipe)
-        {
-            Recipes.Add(recipe);
-        }
+//         public void AddRecipe(Recipe recipe)
+//         {
+//             Recipes.Add(recipe);
+//         }
 
-    }
-}
+//     }
+// }
